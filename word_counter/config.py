891e0c2b4706971f30e854decc7e61b538a32148
rdgozum <ryanpaul.gozum@gmail.com>
2021-01-25 22:20:13 +0800

    Set the app to DEBUG mode

--- word_counter/config.py
+++ word_counter/config.py
@@ -6,5 +6,6 @@ load_dotenv()
 
 class Config:
     FLASK_APP = os.environ.get("FLASK_APP")
+    FLASK_DEBUG = os.environ.get("FLASK_DEBUG")
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
