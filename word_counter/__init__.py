d750e39ede95063cb5bcdd663a57a5d755e5166b
rdgozum <ryanpaul.gozum@gmail.com>
2021-01-25 22:22:21 +0800

    Implement application factory

--- word_counter/__init__.py
+++ word_counter/__init__.py
@@ -1,5 +1,20 @@
-"""Top-level package for word-counter."""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-__author__ = """Ryan Paul Gozum"""
-__email__ = "ryanpaul.gozum@gmail.com"
-__version__ = "0.1.0"
+db = SQLAlchemy()
+
+
+def create_app(config_class):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+
+    with app.app_context():
+        from word_counter.main.routes import main
+        from word_counter.errors.handlers import errors
+
+        app.register_blueprint(main)
+        app.register_blueprint(errors)
+        db.create_all()
+
+        return app
